fa37bc182e117e756f13d158212fb4ec10197174
mrobistore <mrobistore@gmail.com>
2024-11-12 05:30:03 +0700

    Create main.js

---
+++ js/main.js
@@ -0,0 +1,17 @@
+// JavaScript untuk slider otomatis
+let currentSlide = 0;
+const slides = document.querySelectorAll('.slide');
+
+function showSlide(index) {
+    slides.forEach((slide, i) => {
+        slide.classList.remove('active');
+        if (i === index) slide.classList.add('active');
+    });
+}
+
+function nextSlide() {
+    currentSlide = (currentSlide + 1) % slides.length;
+    showSlide(currentSlide);
+}
+
+setInterval(nextSlide, 3000); // Slide akan berubah setiap 3 detik
